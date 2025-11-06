c46fc0362d74f709495847de9a9b915f200c3dbc
차재민 <chajaemin@chajaemin-ui-MacBookPro.local>
2025-11-06 19:17:40 +0900

    fix: 괄호 매칭 오류 수정
    RefreshIndicator의 닫는 괄호 누락으로 인한 컴파일 오류 해결

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -719,6 +719,7 @@ class _MeetupHomePageState extends State<MeetupHomePage>
                     );
                   },
                 ),
+                    ),
               ),
             ),
           ),
